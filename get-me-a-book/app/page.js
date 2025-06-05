9e4a609d5a8a5d9af7284a0b3f65c04cd1138a9c
FajilChauhan <chauhanfajil07@gmail.com>
2025-06-05 15:59:56 +0530

    login page

--- get-me-a-book/app/page.js
+++ get-me-a-book/app/page.js
@@ -4,26 +4,27 @@ export default function Home() {
   return (
     <>
       <div className="flex justify-center flex-col gap-4 items-center text-black h-[44vh]">
-        <div className="font-bold text-5xl">
-          Buy Me a Book
+        <div className="font-bold text-5xl flex justify-center items-center">
+          <span className="mx-4">Buy Me a Book</span>
+          <img src="book.webp" width={55} alt="" />
         </div>
         <p>
           A crowdfunding platform for Who is not able to buy a book. Let's
           Part For A Helping A child For Study. Start now!
         </p>
         <div>
-          <button type="button" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+          <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
             Start Here
           </button>
-          <button type="button" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+          <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
             Read More
           </button>
         </div>
       </div>
       <div className="bg-black h-1 opacity-10">
       </div>
-      <div className="text-black container mx-auto">
-        <h1 className="text-2xl font-bold text-center my-4">Your Fans can buy you a Book</h1>
+      <div className="text-black container mx-auto my-4">
+        <h1 className="text-3xl font-bold text-center my-4">Your Fans can buy you a Book</h1>
         <div className="flex gap-5 justify-around">
           <div className="item space-y-3 flex flex-col items-center justify-center">
             <img className="bg-slate-300 rounded-full p-2" width={100} src="\main.jpeg" alt="" />
@@ -43,26 +44,23 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="bg-black h-1 opacity-10 my-4">
+      <div className="bg-black h-1 opacity-10">
       </div>
-      <div className="text-black container mx-auto">
-        <h1 className="text-2xl font-bold text-center my-4">Your Fans can buy you a Book</h1>
+      <div className="text-black container mx-auto flex flex-col items-center justify-center">
+        <h1 className="text-3xl font-bold text-center my-4">Learn More About Us</h1>
         <div className="flex gap-5 justify-around">
-          <div className="item space-y-3 flex flex-col items-center justify-center">
-            <img className="bg-slate-300 rounded-full p-2" width={100} src="\main.jpeg" alt="" />
-            <p className="font-bold">Fans Want to help</p>
-            <p className="text-center">Your fans are available for you to help you</p>
+          <div className="space-y-3 flex flex-col justify-center items-center mx-14 border-2 border-black bg-green-200">
+            <p className="font-bold text-lg">Contact Us</p>
+            <p className="text-center flex">MYSY Help-Line No: <h1 className="text-blue-900">7043333181</h1></p>
+            <p className="text-center flex">Kcg Emailid :- <h1 className="text-blue-900">mysy-kcg@gujgov.edu.in</h1></p>
+            <p className="text-center">Follow us on Instagram for regular updates.</p>
+            <p className="text-center flex">Instagram id- <h1 className="text-blue-900">mysykcg</h1></p>
           </div>
-          <div className="item space-y-3 flex flex-col items-center justify-center">
-            <img className="bg-slate-300 rounded-full p-2" width={100} src="\main.jpeg" alt="" />
-            <p className="font-bold">Fans Want to help</p>
-            <p className="text-center">Your fans are available for you to help you</p>
-          </div>
-          <div className="item space-y-3 flex flex-col items-center justify-center">
-            <img className="bg-slate-300 rounded-full p-2" width={100} src="\main.jpeg" alt="" />
-            <p className="font-bold">Fans Want to help</p>
-            <p className="text-center">Your fans are available for you to help you</p>
-          </div>
+            <iframe width="400" height="300" src="https://www.youtube.com/embed/kxGZekqJA30?si=9t-7biMSq5uj0VeM" 
+        title="YouTube video player" frameborder="0" allow="accelerometer; 
+        autoplay; clipboard-write; encrypted-media; gyroscope; 
+        picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" 
+        allowfullscreen></iframe>
         </div>
       </div>
     </>
--- get-me-a-book/components/Navbar.js
+++ get-me-a-book/components/Navbar.js
@@ -1,19 +1,20 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
     <nav className='bg-blue-300 text-black 
-        flex justify-between items-center px-4 h-10'>
-      <div className="logo font-bold text-lg">
-        Getme-A-Book!
+        flex justify-between items-center px-4 h-16'>
+      <div className="logo font-bold text-lg flex justify-center items-center">
+        <img src="book.webp" width={30} alt="" />
+        <span>Getme-A-Book!</span>
       </div>
-      <ul className='flex justify-between gap-4'>
-        <li>Home</li>
-        <li>About</li>
-        <li>Project</li>
-        <li>Sign Up</li>
-        <li>Login</li>
-      </ul>
+      <div>
+        <Link href={"/login"}>
+        <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+            Login
+        </button></Link>
+      </div>
     </nav>
   )
 }
